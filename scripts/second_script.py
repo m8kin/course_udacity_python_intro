005010ea8ad757758fa8347811a7653a928bf04a
m8kin <markaitkin@gmail.com>
2020-05-14 21:11:38 +1000

    scripting

---
+++ scripts/second_script.py
@@ -0,0 +1,15 @@
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string)
